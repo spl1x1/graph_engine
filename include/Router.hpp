5ba9e4de8d66c0112585d295fcea373785d457a6
Lukáš Kaplánek <146857173+spl1x1@users.noreply.github.com>
2026-06-13 21:39:03 +0200

    refactor: Add LSDB composition to Router class with topology management methods

--- include/Router.hpp
+++ include/Router.hpp
@@ -3,10 +3,12 @@
 
 #include "Node.hpp"
 #include "Vector.hpp"
+#include "LSDB.hpp"
 
 class Router: public INode{
     NodePosition Position;
     NodeData Data;
+    LSDB topology;  // Composed LSDB for topology management
 
 public:
     Router(Vec2 Position) : Position{Position}, Data{.Id = 0, .Type = "Router"} {};
@@ -21,6 +23,13 @@ public:
     virtual void PushMessage(Message message) override;
     virtual NodeData& GetData() override;
     virtual void NodeClicked() override;
+    
+    // LSDB-related methods
+    LSDB& GetLSDB();
+    const LSDB& GetLSDB() const;
+    void InitializeLSDB();
+    void PrintTopologyDatabase() const;
+    void PrintLSDBStatistics() const;
 };
 
 
